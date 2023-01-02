07f7dcd7fb709bfec20cf20bedd47d9fdbf9bcfb
edwardash <edwardash00@gmail.com>
2023-01-02 22:30:44 +0530

    CSS issue solved

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -3,14 +3,17 @@ import React from 'react'
 import ScrollToBottom from 'react-scroll-to-bottom';
 import Message from './Message/Message'
 import './Messages.css'
+
 const Messages = ({ messages, name }) => {
     return (
         <>
-            <div className='ht-74'>
+            <div className='msg'>
 
                 <ScrollToBottom className="messages">
                     {messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
                 </ScrollToBottom>
+
+
             </div>
 
         </>
